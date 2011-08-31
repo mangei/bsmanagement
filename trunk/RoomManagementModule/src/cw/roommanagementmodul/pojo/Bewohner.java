5a9df954c1b0b64ff910f564d1b288a2ad0a9b41
mangei <mangei@bf19b2dd-5cd8-4513-9558-a7cf39fbeb78>
2011-08-31 20:11:39 +0000

    Merged continued project

--- trunk/RoomManagementModule/src/cw/roommanagementmodul/pojo/Bewohner.java
+++ trunk/RoomManagementModule/src/cw/roommanagementmodul/pojo/Bewohner.java
@@ -1,18 +1,23 @@
 package cw.roommanagementmodul.pojo;
 
-import com.jgoodies.binding.beans.Model;
-import cw.boardingschoolmanagement.interfaces.AnnotatedClass;
-import cw.customermanagementmodul.pojo.Customer;
 import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.jgoodies.binding.beans.Model;
+
+import cw.boardingschoolmanagement.interfaces.AnnotatedClass;
+import cw.customermanagementmodul.pojo.Customer;
 
 
 /**
@@ -90,6 +95,7 @@ public class Bewohner
     }
 
     @OneToOne(cascade=javax.persistence.CascadeType.ALL)
+    @OnDelete(action=OnDeleteAction.CASCADE)
     public Customer getCustomer() {
         return customer;
     }
