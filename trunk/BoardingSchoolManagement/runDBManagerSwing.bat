6ea246b9eb77d71bd502b1a7e9cb9552809e49c8
mangei <mangei@bf19b2dd-5cd8-4513-9558-a7cf39fbeb78>
2011-09-14 16:03:55 +0000

    Commit current state during master refactoring

--- trunk/BoardingSchoolManagement/runDBManagerSwing.bat
+++ trunk/BoardingSchoolManagement/runDBManagerSwing.bat
@@ -1,1 +1,2 @@
-@java -classpath lib\hsqldb.jar org.hsqldb.util.DatabaseManagerSwing %1 %2 %3 %4 %5 %6 %7 %8 %9
+"C:\Program Files (x86)\Java\jre7\bin\java" -classpath lib\hsqldb.jar org.hsqldb.util.DatabaseManagerSwing %1 %2 %3 %4 %5 %6 %7 %8 %9
+pause
